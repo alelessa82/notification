02f09b190da84fc923185e051630cbf1d672e79a
Alexandre Oliveira <51968191+alelessa82@users.noreply.github.com>
2020-08-12 08:03:20 -0300

    Update index.php
    update library

--- teste/index.php
+++ teste/index.php
@@ -6,6 +6,7 @@ require __DIR__ . '/../lib_ext/autoload.php';
 use Notification\Email;
 
 
-$novoEmail = new Email('2', "email-ssl.com.br","auriquimica@auriquimica.com.br","@st3r1x@st3r1X","PHPMailer::ENCRYPTION_STARTTLS","587","auriquimica@auriquimica.com.br","Teste de email da notification");
-$novoEmail->sendMail( "Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b></p>", "backup@samech.inf.br", "Alexandre L. Oliveira", "ale.lessa82@gmail.com", "Alexandre Lessa");
-var_dump($novoEmail);
+$email = new Email(2, "mail.host.com","your@email.com","your-pass","smtp secure (ENCRYPTION_STARTTLS/ENCRYPTION_SMTPS)","port (587/465)","from@email.com","From Name");
+$email-> sendEmail ("Subject","Content","reply@email.com","ReplayName","address@email.com", "Address Name");
+
+var_dump($email);
